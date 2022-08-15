980602f635b5f9ab158e37b21c4a5a8c7dbcd447
michael-twomey <michael-twomey1@outlook.com>
2022-08-15 15:45:57 -0400

    Change txt in console.log statement at top of file

--- populatedb-revised.js
+++ populatedb-revised.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-console.log('This script populates your db with authors, genres, books, and book instances. Specify db connection url as arg (e.g. node popdb.js mongodb+srv://db-username:db-password@cluster0.uwwlz8f.mongodb.net/local-library?retryWrites=true&w=majority');
+console.log('This script populates your db with authors, genres, books, and book instances. Specify db connection url as arg (e.g. node populatedb-revised.js mongodb+srv://db-username:db-password@cluster0.uwwlz8f.mongodb.net/local-library?retryWrites=true&w=majority');
 const userArgs = process.argv.slice(2);
 
 const mongoose = require('mongoose');
